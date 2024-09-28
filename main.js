971e1f870829b0f0b1bfd7971e4e7c290ae58d01
GeonWoo-LPS <kimgeonwoo17@gmail.com>
2024-09-28 21:31:37 +0900

    주석

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ let products = [
 let selectNode = document.getElementById('select');
 let resultNode = document.getElementById('result');
 
+// products 데이터로 option 부분 생성
 function option() {
   products.forEach((product) => {
     let option = document.createElement('option');
@@ -24,6 +25,7 @@ function option() {
 }
 option();
 
+// 결과창에 선택된 option 표시
 function result() {
   let productList = [];
   let selectedProduct = document.querySelectorAll('#select option:checked');
@@ -31,6 +33,7 @@ function result() {
     productList.push(option);
   });
 
+  // 재실행 될 때마다 result 부분 초기화
   resultNode.innerHTML = '';
 
   let shoppingCart = document.createElement('h3');
@@ -53,9 +56,9 @@ function result() {
   sum.appendChild(document.createTextNode(`총액 : ${itemSum}`));
   resultNode.appendChild(sum);
 }
-
 selectNode.addEventListener('change', result);
 
+// 결제하기 btn 클릭 시 결제창을 새 창으로 open
 function openPayment() {
   let itemSum = 0;
   let selectedProduct = document.querySelectorAll('#select option:checked');
@@ -64,7 +67,8 @@ function openPayment() {
     itemSum += parseInt(option.value);
   });
 
-  if (!resultNode.innerHTML) {
+  // result가 공백일 경우 alert 띄움
+  if (resultNode.innerHTML.trim() === '') {
     alert('결제할 상품을 선택해야 합니다.');
   } else {
     let paymentWindow = window.open(
@@ -76,6 +80,7 @@ function openPayment() {
   }
 }
 
+// 결제가 끝나고 result 부분 초기화
 function successPayment() {
   resultNode.innerHTML = '';
 }
